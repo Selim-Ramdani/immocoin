0f04c845c308e03b754e5fa2b25774413bd511f4
Selim-Ramdani <selimcodeur@gmail.com>
2022-12-13 19:36:06 +0100

    Add BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./assets/fonts/stylesheet.css";
 import "./assets/styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
